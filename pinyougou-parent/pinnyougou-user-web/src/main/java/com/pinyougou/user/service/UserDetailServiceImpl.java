8eaa075103f6ab229dcf6850054a33d7b5b7cb66
Reolcharm <reolcharm@gmail.com>
2018-12-14 17:17:18 +0800

    day159-15: 初步集成了 CAS  和 Spring Security 到项目中, 本地CAS服务也搭建起来了. todo:     - 登陆页面的"换皮手术"     - 测试.

---
+++ pinyougou-parent/pinnyougou-user-web/src/main/java/com/pinyougou/user/service/UserDetailServiceImpl.java
@@ -0,0 +1,27 @@
+package com.pinyougou.user.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * @author Reolcharm
+ * @description 返回认证权限, username 和 password 已交给 CAS 了.
+ */
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		List<GrantedAuthority> authorities = new ArrayList();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		return new User(username, "", authorities);
+	}
+
+}
